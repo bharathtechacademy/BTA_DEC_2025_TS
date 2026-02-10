b47ea596fcd104a4ffbe9fa0060c08253f6c7217
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-02-10 10:37:01 +0530

    Browser-Actions

---
+++ playwright-training/tests/5-playwright-locators/locators.spec.ts
@@ -0,0 +1,14 @@
+//WebElement : A web element is nothing but the element displayed within the UI. (e.g. text box, button, link, dropdown etc.)
+//Locator: Locator is a default play rate method to identify the location of web elements within the web page. 
+
+//There are multiple locators available in Playwright to locate the elements. 
+
+//1.getByRole
+//2.getByLabel
+//3.getByText
+//4.getByPlaceholder
+//5.getByAltText
+//6.getByTitle
+//7.getByTestId
+//8.locator (css)
+//9.locator (xpath)
